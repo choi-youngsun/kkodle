48148e2c23e7e68975947a36b496289e2482eb4c
MELATONIN99 <sindogfood@naver.com>
2024-10-03 06:06:21 +0900

    feat: 툴바, 모달, 버튼 구현 * 비어있는 모달 내용은 추후 추가 예정입니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,78 @@
+import { useCallback, useEffect, useState } from 'react';
+import './App.css';
+import { createClient } from '@supabase/supabase-js';
+import dayjs from 'dayjs';
+import ToolBar from './components/ToolBar.tsx';
 import LetterRow from './components/LetterRow.tsx';
 import SubmitLetterRow from './components/SubmitLetterRow.tsx';
 import KeyBoard from './components/KeyBoard.tsx';
 
-function App() {
-  type LetterStatus = 'default' | 'ball' | 'strike' | 'error';
+export type LetterStatus = 'default' | 'ball' | 'strike' | 'error';
 
-  type Letter = {
-    letter: string;
-    status: LetterStatus;
-  };
+export type Letter = {
+  letter: string;
+  status: LetterStatus;
+};
+interface GameState {
+  guesses: string[];
+  solution: string;
+}
+
+const supabaseUrl = 'https://yznhshnhrfruzomamffs.supabase.co';
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY!;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+function App() {
+  const [timeState, setTimeState] = useState<string>(() => {
+    const savedTimeState = window.localStorage.getItem('timeState');
+    return savedTimeState ? JSON.parse(savedTimeState) : '';
+  });
+  const [gameState, setGameState] = useState<GameState>(() => {
+    const savedGameState = JSON.parse(
+      window.localStorage.getItem('gameState') || '{}'
+    );
+    return savedGameState.solution
+      ? savedGameState
+      : { guesses: [], solution: '' };
+  });
+
+  const getRandomQuestion = useCallback(
+    async (attempt = 1) => {
+      const MAX_ATTEMPTS = 3;
+      if (attempt > MAX_ATTEMPTS) return;
+      const { data, error } = await supabase.rpc('get_random_question');
+      if (error) {
+        // eslint-disable-next-line no-console
+        console.error('질문을 가져오는 중 오류 발생:', error);
+        return;
+      }
+
+      const questionText = data[0]?.question;
+      if (questionText === gameState.solution) {
+        setTimeout(() => getRandomQuestion(attempt + 1), 1000);
+      } else {
+        setGameState((prevState) => ({
+          ...prevState,
+          solution: questionText,
+        }));
+      }
+    },
+    [gameState.solution]
+  );
+
+  useEffect(() => {
+    // TODO: 테스트를 위해 임시 속성임, 추후 1시간 혹은 2시간으로 수정예정
+    const now = dayjs().format('YY-MM-DD HH:mm');
+    if (timeState !== now) {
+      setTimeState(now);
+    }
+    window.localStorage.setItem('timeState', JSON.stringify(now));
+    window.localStorage.setItem('gameState', JSON.stringify(gameState));
+    if (timeState !== now) {
+      getRandomQuestion();
+    }
+  }, [timeState, gameState, getRandomQuestion]);
 
   const word1 = ['ㄱ', 'ㅗ', 'ㅏ', 'ㅈ', 'ㅓ', 'ㅣ'];
   const word2 = ['ㄱ', 'ㅗ', 'ㅈ', 'ㅏ', 'ㅇ'];
@@ -32,6 +96,7 @@ function App() {
 
   return (
     <div>
+      <ToolBar />
       <div>
         <LetterRow inputValue={word1} />
         <SubmitLetterRow inputValue={submitWord1} />
---
+++ src/components/TransitionsModal.tsx
@@ -0,0 +1,153 @@
+import Modal from 'react-modal';
+import styled from 'styled-components';
+import CloseIcon from '../assets/close.svg';
+import { IconImg } from './ToolBar.tsx';
+import SubmitLetterRow from './SubmitLetterRow.tsx';
+import { Letter } from '../App.tsx';
+
+interface Props {
+  isModalOpen: boolean;
+  handleClose: () => void;
+  modalType: string;
+}
+
+const customStyles = {
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+  },
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    padding: 0,
+    border: 0,
+  },
+};
+
+const Wrapper = styled.div`
+  background-color: white;
+  width: 350px;
+  height: auto;
+  padding: 1.2rem;
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: 0;
+  right: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: inherit;
+  border: none;
+  box-shadow: none;
+  border-radius: 0;
+  overflow: visible;
+  cursor: pointer;
+  padding: 0;
+  margin: 0.5rem;
+`;
+const ModalText = styled.p`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  flex-direction: column;
+  margin: 0;
+`;
+const ModalTitle = styled.h2`
+  margin: 0.5rem 0 1rem 0;
+`;
+const ModalMiniText = styled.p`
+  margin: 0 0 1rem 0;
+  font-size: 0.9rem;
+  color: gray;
+`;
+export default function TransitionsModal({
+  isModalOpen,
+  handleClose,
+  modalType,
+}: Props) {
+  const renderModalContent = () => {
+    const submitWord1: Letter[] = [
+      { letter: 'ㄱ', status: 'default' },
+      { letter: 'ㅗ', status: 'ball' },
+      { letter: 'ㅏ', status: 'ball' },
+      { letter: 'ㅈ', status: 'strike' },
+      { letter: 'ㅓ', status: 'default' },
+      { letter: 'ㅣ', status: 'bell' },
+    ];
+    switch (modalType) {
+      case 'Beaker':
+        return <Wrapper>Beaker</Wrapper>;
+      case 'Cog':
+        return <Wrapper>Cog</Wrapper>;
+      case 'Question':
+        return (
+          <Wrapper>
+            <ModalText>
+              <ModalTitle>어떻게 할까?</ModalTitle>
+              <ModalMiniText>
+                여섯 개의 자모로 풀어쓴 한글 단어 꼬들 을 여섯 번의
+                <br />
+                도전 안에 맞혀봅시다. 한글 단어를 풀어쓴 후<br />
+                <strong>입력</strong>을 누르면 칸 색깔이 변합니다!
+              </ModalMiniText>
+              <SubmitLetterRow inputValue={submitWord1} />
+              <ModalMiniText>
+                자음 &apos;ㅈ&apos;는 올바른 자리에 있습니다. <br />
+                모음 &apos;ㅗ&apos;,&apos;ㅏ&apos;는 잘못된 자리에 있습니다.{' '}
+                <br />
+                자음 &apos;ㄱ&apos;, 모음&apos;ㅓ&apos;,&apos;ㅣ&apos;는 어느
+                곳에도 맞지 않습니다.
+                <br />
+                복합모음과 쌍자음, 겹받침은 더 작은 자모들로 풀어집니다.
+              </ModalMiniText>
+            </ModalText>
+          </Wrapper>
+        );
+      case 'Chart':
+        return <Wrapper>Chart</Wrapper>;
+      case 'AboutGame':
+        return (
+          <Wrapper>
+            <ModalText>
+              <ModalTitle>이 놀이는?</ModalTitle>
+              이 놀이는 Belorin & Silas가 한국어로 만든
+              <br />
+              오픈 소스 프로젝트를 기반으로 <br />
+              구현한 게임입니다.
+              <br />
+              원본 한국어 워들 게임은
+              <a
+                href="https://kordle.kr/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                꼬들
+              </a>
+              에서 즐기실 수 있습니다.
+            </ModalText>
+          </Wrapper>
+        );
+      default:
+        return <Wrapper>NULL</Wrapper>;
+    }
+  };
+
+  return (
+    <Modal
+      style={customStyles}
+      isOpen={isModalOpen}
+      onRequestClose={() => handleClose()}
+    >
+      <CloseButton onClick={() => handleClose()}>
+        <IconImg src={CloseIcon} alt="close-icon" />
+      </CloseButton>
+      {renderModalContent()}
+    </Modal>
+  );
+}
