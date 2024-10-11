ba37c1243d7747316c97154b402f5d9a38b3c1f9
Choi Young Sun <sunn7931@naver.com>
2024-10-11 16:57:40 +0900

    feat: 테스트 데이터로 통계 모달 UI 완성

---
+++ src/components/ChartBar.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+const StyledBar = styled.div<{ rate: number }>`
+  background-color: #2563eb;
+  height: 18px;
+  border-radius: 40px 0 0 40px;
+  width: ${({ rate }) => `${rate}%`};
+`;
+
+const StyledCount = styled.p`
+  color: #ffffff;
+  text-align: center;
+  font-size: 13px;
+  margin: 0;
+`;
+
+type ChartBarProps = {
+  count: number;
+  rate: number;
+};
+
+export default function ChartBar({ count, rate }: ChartBarProps) {
+  return (
+    <StyledBar rate={rate}>
+      <StyledCount>{count}</StyledCount>
+    </StyledBar>
+  );
+}
--- src/components/ChartModal.tsx
+++ src/components/ChartModal.tsx
@@ -1,53 +1,130 @@
 import styled from 'styled-components';
+import ChartBar from './ChartBar.tsx';
+import CountToPercent from './CountToPercent.ts';
+
+const StyledMainSection = styled.div``;
 
 const StyledTitle = styled.h1`
-  font-size: 23px;
+  font-size: 20px;
   text-align: center;
 `;
 
 const StyledTopSection = styled.div`
   display: flex;
+  gap: 5px;
+  margin-bottom: 30px;
 `;
 
 const StyledColumn = styled.div`
   display: flex;
   flex-direction: column;
+  text-align: center;
+  width: 83px;
+  flex-shrink: 0;
 `;
+
+const StyledNumber = styled.p`
+  font-size: 30px;
+  font-weight: 700;
+  margin: 0 auto;
+`;
+
+const StyledIndexNumber = styled.p`
+  font-size: 13px;
+  font-weight: 500;
+  margin: 0;
+`;
+
+const StyledText = styled.p`
+  font-size: 13px;
+  margin: 0 auto;
+`;
+
+const StyledChartBarSection = styled.div`
+  display: flex;
+  gap: 10px;
+  margin-bottom: 5px;
+`;
+
+const StyledMiddleSection = styled.div`
+  margin: 0 10px;
+`;
+
+const StyledTimeSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`;
+
+const StyleBottomSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 30px 0 10px 0;
+`;
+
+const StyledTimeText = styled.p`
+  font-size: 17px;
+  margin: 0 auto;
+`;
+
+const StyledTimeNumber = styled.p`
+  font-size: 18px;
+  margin: 0 auto;
+`;
+
+const StyledButton = styled.button`
+  border-radius: 10px;
+  background-color: #4f46e5;
+  flex-grow: 1;
+  color: #ffffff;
+  height: 40px;
+  border: none;
+  cursor: pointer;
+`;
+
+const inputArray = [100, 30, 20, 40, 50, 30];
+const percentageArray = CountToPercent(inputArray);
+const generateUniqueKey = () => crypto.randomUUID();
 
 export default function ChartModal() {
   return (
-    <div>
+    <StyledMainSection>
       <StyledTitle>통계</StyledTitle>
       <StyledTopSection>
         <StyledColumn>
-          <p>1</p>
-          <p>전체 도전</p>
+          <StyledNumber>1</StyledNumber>
+          <StyledText>전체 도전</StyledText>
         </StyledColumn>
         <StyledColumn>
-          <p>100%</p>
-          <p>정답률</p>
+          <StyledNumber>100%</StyledNumber>
+          <StyledText>정답률</StyledText>
         </StyledColumn>
         <StyledColumn>
-          <p>1</p>
-          <p>최근 연속 정답</p>
+          <StyledNumber>1</StyledNumber>
+          <StyledText>최근 연속 정답</StyledText>
         </StyledColumn>
         <StyledColumn>
-          <p>1</p>
-          <p>최다 연속 정답</p>
+          <StyledNumber>1</StyledNumber>
+          <StyledText>최다 연속 정답</StyledText>
         </StyledColumn>
       </StyledTopSection>
-      <p>도전 분포</p>
-      <div>
-        <div>프로그래스바</div>
-        <div>프로그래스바</div>
-        <div>프로그래스바</div>
-        <div>프로그래스바</div>
-        <div>프로그래스바</div>
-        <div>프로그래스바</div>
-      </div>
-      <p>새로운 문제까지</p>
-      <p>09:13:30</p>
-      <button type="button">결과 복사</button>
-    </div>
+      <StyledTitle>도전 분포</StyledTitle>
+      <StyledMiddleSection>
+        {inputArray.map((count, index) => (
+          <StyledChartBarSection key={generateUniqueKey()}>
+            <StyledIndexNumber>{index + 1}</StyledIndexNumber>
+            <ChartBar rate={percentageArray[index]} count={count} />
+          </StyledChartBarSection>
+        ))}
+      </StyledMiddleSection>
+      <StyleBottomSection>
+        <StyledTimeSection>
+          <StyledTimeText>새로운 문제까지</StyledTimeText>
+          <StyledTimeNumber>09:13:30</StyledTimeNumber>
+        </StyledTimeSection>
+        <StyledButton type="button">결과 복사</StyledButton>
+      </StyleBottomSection>
+    </StyledMainSection>
   );
 }
